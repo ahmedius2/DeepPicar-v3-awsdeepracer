0dbcab584328f8eb0a027cab5c88355c3d12a3ef
Heechul Yun <heechul.yun@ku.edu>
2024-04-26 14:41:17 -0400

    direct pwm output

--- actuator-servo-deepracer.py
+++ actuator-servo-deepracer.py
@@ -1,12 +1,51 @@
 # init
+
+THROTTLE_PWM_FILE = "/sys/class/pwm/pwmchip0/pwm0/duty_cycle"
+STEERING_PWM_FILE = "/sys/class/pwm/pwmchip0/pwm1/duty_cycle"
+
+# throttle range:
+# 1046000               max forward
+# 1446000               stop
+# 1846000               max reverse
+
+# steering range:
+# 1046000               right max
+# 1446000               center
+# 1846000               left max
+
 def init(default_speed=50):
     return
 
 def set_throttle(throttle_pct):
-    return
+    # throttle_pct: [-100, +100]
+    # throttle_pwm: [1046000, 1846000]
+    throttle_pwm = int(1446000 + (throttle_pct * 40000 / 100))
+    print(f"throttle_pct: {throttle_pct} throttle_pwm: {throttle_pwm}")
+
+    # Ensure throttle_pwm is within the acceptable range
+    throttle_pwm = max(1046000, min(1846000, throttle_pwm))
+    try:
+        with open(THROTTLE_PWM_FILE, 'w') as f:
+            f.write(str(throttle_pwm))
+    except OSError as e:
+        print(f"Failed to set throttle: {e}")
 
 def set_steering(steering_deg):
-    return
+    # steering_deg: [-90, +90]
+    # steering_pwm: [1046000, 1846000]
+    steering_pwm = int(1446000 + (steering_deg * 40000 / 90))
+    print(f"steering_deg: {steering_deg} steering_pwm: {steering_pwm}")
+
+    # Ensure steering_pwm is within the acceptable range
+    steering_pwm = max(1046000, min(1846000, steering_pwm))
+    try:
+        with open(STEERING_PWM_FILE, 'w') as f:
+            f.write(str(steering_pwm))
+    except OSError as e:
+        print(f"Failed to set steering: {e}")
 
 def stop():
-    return
+    with open(THROTTLE_PWM_FILE, 'w') as f:
+        f.write("1446000")
+    with open(STEERING_PWM_FILE, 'w') as f:
+        f.write("1446000")
